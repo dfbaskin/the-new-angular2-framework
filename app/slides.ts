8711447a506918e759946bf79841320429f25265
Dave Baskin <dbaskin@wintellect.com>
2016-06-14 21:32:53 -0400

    Adding content pages.

--- app/slides.ts
+++ app/slides.ts
@@ -3,10 +3,12 @@ import {TitlePageComponent} from './slides/title-page/title-page.component';
 import {ClosingPageComponent} from './slides/closing-page/closing-page.component';
 import {OverviewPageComponent} from './slides/overview-page/overview-page.component';
 import {ReactOverviewComponent} from './slides/react-overview/react-overview.component';
+import {GeneralDifferencesComponent} from './slides/general-differences/general-differences.component';
             
 export const SLIDE_COMPONENTS = [
     TitlePageComponent,
     ClosingPageComponent,
     OverviewPageComponent,
-    ReactOverviewComponent
+    ReactOverviewComponent,
+    GeneralDifferencesComponent
 ];
---
+++ app/slides/general-differences/general-differences.component.ts
@@ -0,0 +1,10 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'general-differences',
+    styleUrls: ['general-differences.component.css'],
+    templateUrl: 'general-differences.component.html'
+})
+export class GeneralDifferencesComponent { }
